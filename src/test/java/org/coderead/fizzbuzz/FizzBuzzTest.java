caa61dbcfc8927a67b3d39c5d7df710e9aac8bda
huisheng.jin <huisheng.jinge@gmail.com>
2021-04-24 12:13:12 +0800

    refactor

--- src/test/java/org/coderead/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/coderead/fizzbuzz/FizzBuzzTest.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 
 
 public class FizzBuzzTest {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZBUZZ = "FizzBuzz";
+
+
     @Test
     public void should_print_itself() {
         assertEqual(1, "1");
@@ -13,45 +18,46 @@ public class FizzBuzzTest {
 
     @Test
     public void should_print_Fizz_given_number_divided_by_3() {
-        assertEqual(3, "Fizz");
-        assertEqual(6, "Fizz");
+        assertEqual(3, FIZZ);
+        assertEqual(6, FIZZ);
     }
 
     @Test
     public void should_print_Fizz_given_number_contains_3() {
-        assertEqual(13, "Fizz");
-        assertEqual(31, "Fizz");
+        assertEqual(13, FIZZ);
+        assertEqual(31, FIZZ);
     }
 
     @Test
     public void should_print_Buzz_given_number_divided_by_5() {
-        assertEqual(5, "Buzz");
-        assertEqual(10, "Buzz");
+        assertEqual(5, BUZZ);
+        assertEqual(10, BUZZ);
     }
 
     @Test
     public void should_print_Buzz_given_number_contains_5() {
-        assertEqual(52, "Buzz");
+        assertEqual(52, BUZZ);
     }
 
     @Test
     public void should_print_FizzBuzz_given_number_divided_by_3_and_5() {
-        assertEqual(15, "FizzBuzz");
-        assertEqual(45, "FizzBuzz");
+        assertEqual(15, FIZZBUZZ);
+        assertEqual(45, FIZZBUZZ);
     }
 
     @Test
     public void should_print_FizzBuzz_given_number_contains_3_and_5() {
-        assertEqual(35, "FizzBuzz");
+        assertEqual(35, FIZZBUZZ);
     }
 
     @Test
     public void should_print_FizzBuzz_given_number_contains_3_and_divided_by_5() {
-        assertEqual(310, "FizzBuzz");
+        assertEqual(310, FIZZBUZZ);
     }
+
     @Test
     public void should_print_FizzBuzz_given_number_divided_by_3_and_contains_5() {
-        assertEqual(51, "FizzBuzz");
+        assertEqual(51, FIZZBUZZ);
     }
 
     private void assertEqual(int number, String expected) {
